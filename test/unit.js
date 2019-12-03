62906ce723536f8147b70e1a6984eb1f355b66f3
Luke Childs <lukechilds123@gmail.com>
2019-12-03 13:17:22 +0700

    Test bip39v.generateMnemonic() defaults to P2PKH version byte

--- test/unit.js
+++ test/unit.js
@@ -16,3 +16,10 @@ test('bip39v.generateMnemonic(length) generates different length mnemonics', t =
 	t.is(bip39v.generateMnemonic(224).split(' ').length, 21);
 	t.is(bip39v.generateMnemonic(256).split(' ').length, 24);
 });
+
+test('bip39v.generateMnemonic() defaults to P2PKH version byte', t => {
+	const mnemonic = bip39v.generateMnemonic();
+	const versionByte = bip39v.mnemonicToVersionByte(mnemonic);
+	t.is(versionByte, bip39v.versions.P2PKH);
+	t.is(versionByte, 0x00);
+});
