338c31175ed632c87a6ded7a4fc22cc2f0cb3f89
Luke Childs <lukechilds123@gmail.com>
2019-12-03 13:11:08 +0700

    Test bip39v.generateMnemonic() defaults to 12 words

--- test/unit.js
+++ test/unit.js
@@ -4,3 +4,7 @@ import bip39v from '..';
 test('bip39v is exported', t => {
 	t.not(bip39v, undefined);
 });
+
+test('bip39v.generateMnemonic() defaults to 12 words', t => {
+	t.is(bip39v.generateMnemonic().split(' ').length, 12);
+});
